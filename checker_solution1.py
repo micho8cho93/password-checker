067d1afcd6cffb4b5fc2738d5ea56dd4aeb47d79
micho8cho93 <182514846+micho8cho93@users.noreply.github.com>
2025-02-13 13:13:10 +0100

    Updated base code for all checker solutions

--- checker_solution1.py
+++ checker_solution1.py
@@ -1,17 +1,38 @@
 #create a password strength checking program
 #the program should check the password and then tell the user if it is weak, medium or strong
+import string
+
+def punctuation(password):
+    for i in password:
+        if i in string.punctuation:
+            return True
+    return False
+
+def numbers(password):
+    for i in password:
+        if i in string.digits:
+            return True
+    return False
+
+def letters(password):
+    for i in password:
+        if i in string.ascii_letters:
+            return True
+        return False
 
 def checker():
     password = input("Enter your password: ")
 
     if len(password) < 6:
         print("Your passwords must be at least 6 characters long")
-    elif len(password) > 6 and password.isalpha():
+    elif len(password) > 6 and letters(password) and numbers(password) == False and punctuation(password) == False:
         print("Your password is weak")
-    elif len(password) > 6 and password.isalnum():
+    elif len(password) > 6 and letters(password) and numbers(password) == True and punctuation(password) == False:
         print("Your password is medium")
-    elif len(password) > 6 and password.isalnum() and password.punctuation():
+    elif len(password) > 6 and letters(password) and numbers(password) == True and punctuation(password) == True:
         print("Your password is strong")
+        
+    return
 
 checker()
 #weak passwords only have characters
--- checker_solution2.py
+++ checker_solution2.py
@@ -1,24 +1,22 @@
 from getpass import getpass
 import keyboard
+import string
 
-#must enter sudo python3 checker_solution2.py
-#must add python and python3 from venv to privacy and secuity -> accessibility
+#must enter sudo python3 checker_solution2.py if admin access required
+#might need to add python and python3 from venv to privacy and secuity -> accessibility
+def strength(password):
+    if len(password) < 6:
+        print("Your passwords must be at least 6 characters long")
+    elif len(password) > 6 and password.isalpha():
+        print("Your password is weak")
+    elif (len(password) > 6 and password.isalnum()) or (len(password) > 6 and string.punctuation(password)):
+        print("Your password is medium")
+    elif len(password) > 6 and password.isalnum() and string.punctuation(password):
+        print("Your password is strong")
+
 def checker():
     user = input("Enter your username: ")
     password = getpass("Enter your password: ")
-    while True:
-        if keyboard.is_pressed('esc'):
-                password = input()
-        elif keyboard.is_pressed('enter'):
-                password = input()
-
-        if len(password) < 6:
-            print("Your passwords must be at least 6 characters long")
-        elif len(password) > 6 and password.isalpha():
-            print("Your password is weak")
-        elif len(password) > 6 and password.isalnum():
-            print("Your password is medium")
-        elif len(password) > 6 and password.isalnum() and password.punctuation():
-            print("Your password is strong")
+    strength(password)
 
 checker()
